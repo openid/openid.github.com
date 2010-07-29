806f4c07d2fb65f01b4a8c19fdffbbe0e85569f3
Will Norris <will@willnorris.com>
2010-07-29 15:31:49 -0700

    Added rdoc.sh script for building ruby docs (not versioned yet)

---
+++ rdoc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Build the HTML documentation for the JanRain PHP OpenID library
+#
+# Usage:
+#  build-docs
+#
+# Must be run from the base of the repository
+
+cd ../ruby-openid/
+
+RDOC_FILES="README INSTALL LICENSE UPGRADE lib/openid examples/README"
+MAIN=README
+rdoc -o ../openid.github.com/docs/ruby/ \
+    --main="$MAIN" \
+    $RDOC_FILES
+
+cd -
